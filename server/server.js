eb0ee1debc34f2238e25e417d092c415371b5715
rok0705 <leeseon0503@gmail.com>
2023-10-20 23:54:16 -0400

    feat: image store backend server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+//////////////////////// BACKEND SERVER ///////////////////////////
+
 const express = require("express");
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
@@ -7,10 +9,22 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) =>
     cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
 });
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (["image/jpeg", "image/jpg", "image/png"].includes(file.mimetype))
+      cb(null, true);
+    else cb("invalid file type", false);
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
 
 const app = express();
 const PORT = 5000;
+
+app.use("/uploads", express.static("uploads"));
 
 app.post("/upload", upload.single("imageTest"), (req, res) => {
   console.log("req.file:", req.file);
