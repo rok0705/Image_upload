5f8c8e22c09a6b683fc80f72c6e686d35beded22
rok0705 <leeseon0503@gmail.com>
2023-10-21 13:23:55 -0400

    setting: setting backend in vercel..

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
 const { v4 } = require("uuid");
+const mime = require("mime-types");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) =>
+    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (["image/jpeg", "image/jpg", "image/png"].includes(file.mimetype))
+      cb(null, true);
+    else cb("invalid file type", false);
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
 
 app.get("/api", (req, res) => {
-  console.log("server received /api request successfully!");
+  console.log("backend : received GET /api HTTP request!");
   const path = `/api/item/${v4()}`;
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
@@ -16,5 +36,10 @@ app.get("/api/item/:slug", (req, res) => {
 });
 
 app.use("/uploads,", express.static("uploads"));
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log("req.file:", req.file);
+  res.json(req.file);
+});
 
 module.exports = app;
