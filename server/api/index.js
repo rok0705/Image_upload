734f690f7cc4b628cd52067cbd7750cce40972e9
rok0705 <leeseon0503@gmail.com>
2023-10-21 12:52:51 -0400

    setting: deploy express in vercel

---
+++ server/api/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const { v4 } = require("uuid");
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+module.exports = app;
