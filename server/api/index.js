e42fdbfe13cc86fa777f7bd45750632b7c46ca9c
rok0705 <leeseon0503@gmail.com>
2023-10-21 12:59:06 -0400

    setting: express in vercel

--- server/api/index.js
+++ server/api/index.js
@@ -9,4 +9,9 @@ app.get("/api", (req, res) => {
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
 module.exports = app;
