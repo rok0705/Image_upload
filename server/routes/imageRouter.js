254efdc25c026060ac17590894fdb3841055bdc3
rok0705 <leeseon0503@gmail.com>
2023-10-24 23:36:57 -0400

    feat: image delete API

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -2,6 +2,10 @@ const { Router } = require("express");
 const imageRouter = Router();
 const Image = require("./../models/Image");
 const { upload } = require("../middleware/imageUpload");
+const fs = require("fs");
+const { promisify } = require("util");
+const fileUnlink = promisify(fs.unlink);
+const mongoose = require("mongoose");
 
 imageRouter.post("/", upload.single("image"), async (req, res) => {
   // 유저 정보, public 유무 확인
@@ -30,9 +34,25 @@ imageRouter.get("/", async (req, res) => {
   res.json(images);
 });
 
-imageRouter.delete("/:imageId", (req, res) => {
+imageRouter.delete("/:imageId", async (req, res) => {
   // 유저 권한 확인
   // 사진 삭제
+  // 1. uploads 폴더에 있는 사진 삭제
+  // 2. mongoDB 에 있는 document 삭제
+  try {
+    console.log(req.params);
+    if (!req.user) throw new Error("Not enough privilege.");
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("Invalid ImageId.");
+
+    const image = await Image.findOneAndDelete({ _id: req.params.imageId });
+    if (!image) return res.json({ message: "The image does not exist." });
+    await fileUnlink(`./uploads/${image.key}`);
+    res.json({ message: `${image.key} deleted successfully.` });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
 });
 
 imageRouter.patch("/:imageId/like", (req, res) => {
