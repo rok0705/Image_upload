01f39d87646f6ad1de816d20675ffa8a16a7e65e
rok0705 <leeseon0503@gmail.com>
2023-10-26 01:02:49 -0400

    feat: frontend like unlike

--- client/src/pages/ImagePage.js
+++ client/src/pages/ImagePage.js
@@ -1,13 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { ImageContext } from "../context/ImageContext";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
 
 const ImagePage = () => {
   const { imageId } = useParams();
-  const { images, myImages } = useContext(ImageContext);
+  const { images, myImages, setImages, setMyImages } = useContext(ImageContext);
+  const [hasLiked, setHasLiked] = useState(false);
+  const [me] = useContext(AuthContext);
+
   const image =
     images.find((image) => imageId === image._id) ||
     myImages.find((image) => imageId === image._id);
+
+  useEffect(() => {
+    if (me && image && image.likes.includes(me.userId)) setHasLiked(true);
+  }, [me, image]);
+
+  const updateImage = (images, image) =>
+    [...images.filter((image) => image._id !== imageId), image].sort(
+      (a, b) =>
+        new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    );
+
+  const onSubmit = async () => {
+    const result = await axios.patch(
+      `/images/${imageId}/${hasLiked ? "unlike" : "like"}`
+    );
+    if (result.data.public) setImages(updateImage(images, result.data));
+    else setMyImages(updateImage(myImages, result.data));
+
+    setHasLiked(!hasLiked);
+  };
 
   if (!image) return <h3>Loading...</h3>;
   return (
@@ -18,6 +43,10 @@ const ImagePage = () => {
         alt={imageId}
         src={`http://localhost:5000/uploads/${image.key}`}
       ></img>
+      <span>좋아요 {image.likes.length}</span>
+      <button style={{ float: "right" }} onClick={onSubmit}>
+        {hasLiked ? "좋아요 취소" : "좋아요"}
+      </button>
     </div>
   );
 };
--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -67,7 +67,7 @@ imageRouter.patch("/:imageId/like", async (req, res) => {
     );
     if (!mongoose.isValidObjectId(image)) throw new Error("Invalid imageId.");
 
-    res.json({ message: `${image.key} received a new like!` });
+    res.json(image);
   } catch (err) {
     console.log(err);
     res.status(400).json({ message: err.message });
@@ -86,7 +86,7 @@ imageRouter.patch("/:imageId/unlike", async (req, res) => {
     );
     if (!mongoose.isValidObjectId(image)) throw new Error("Invalid imageId.");
 
-    res.json({ message: `${image.key} received a dislike.` });
+    res.json(image);
   } catch (err) {
     console.log(err);
     res.status(400).json({ message: err.message });
