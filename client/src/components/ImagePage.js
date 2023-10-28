fb681c45462e62ed4a140aba75f7d3767a9bb481
rok0705 <leeseon0503@gmail.com>
2023-10-28 14:17:49 -0400

    image delete

--- client/src/components/ImagePage.js
+++ client/src/components/ImagePage.js
@@ -3,8 +3,11 @@ import { useParams } from "react-router";
 import { ImageContext } from "../context/ImageContext";
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const ImagePage = () => {
+  const navigate = useNavigate();
   const { imageId } = useParams();
   const { images, myImages, setImages, setMyImages } = useContext(ImageContext);
   const [hasLiked, setHasLiked] = useState(false);
@@ -12,7 +15,7 @@ const ImagePage = () => {
   const image =
     images.find((image) => image._id === imageId) ||
     myImages.find((image) => image._id === imageId);
-  const updateImage = (images, image) =>
+  const refreshImage = (images, image) =>
     [...images.filter((image) => image._id !== imageId), image].sort(
       (a, b) =>
         new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
@@ -21,9 +24,21 @@ const ImagePage = () => {
     const result = await axios.patch(
       `/images/${imageId}/${hasLiked ? "unlike" : "like"}`
     );
-    if (result.data.public) setImages(updateImage(images, result.data));
-    else setMyImages(updateImage(images, result.data));
+    if (result.data.public) setImages(refreshImage(images, result.data));
+    else setMyImages(refreshImage(images, result.data));
     setHasLiked(!hasLiked);
+  };
+  const deleteHandler = async () => {
+    try {
+      if (!window.confirm("Do you want to delete the image?")) return;
+      const result = await axios.delete(`/images/${imageId}`);
+      setImages(images.filter((image) => image._id !== imageId));
+      setMyImages(myImages.filter((image) => image._id !== imageId));
+      toast.success(result.data.message);
+      navigate("/");
+    } catch (err) {
+      toast.error(err.message);
+    }
   };
 
   useEffect(() => {
@@ -40,6 +55,14 @@ const ImagePage = () => {
         src={`http://localhost:5000/uploads/${image.key}`}
       ></img>
       <span>좋아요 {image.likes.length}</span>
+      {me && image.user._id === me.userId && (
+        <button
+          style={{ float: "right", marginLeft: 10 }}
+          onClick={deleteHandler}
+        >
+          삭제
+        </button>
+      )}
       <button onClick={onSubmit} style={{ float: "right" }}>
         {hasLiked ? "좋아요 취소" : "좋아요"}
       </button>
