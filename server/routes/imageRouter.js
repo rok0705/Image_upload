f5b620f77d91296238169e73bb17ca54a2b0e6e5
rok0705 <leeseon0503@gmail.com>
2023-10-28 18:39:15 -0400

    pagination backend

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -34,9 +34,26 @@ imageRouter.post("/", upload.array("image", 5), async (req, res) => {
 });
 
 imageRouter.get("/", async (req, res) => {
-  // public 이미지들만 제공
-  const images = await Image.find({ public: true });
-  res.json(images);
+  try {
+    const { lastid } = req.query;
+    if (lastid && mongoose.isValidObjectId(lastid))
+      throw new Error("invalid lastid.");
+    const images = await Image.find(
+      lastid
+        ? {
+            public: true,
+            _id: { $lt: lastid },
+          }
+        : {
+            public: true,
+          }
+    )
+      .sort({ _id: -1 })
+      .limit(20);
+    res.json(images);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 imageRouter.delete("/:imageId", async (req, res) => {
