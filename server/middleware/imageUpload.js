8754e7645229b6d5a051745b36c069be20cdf776
rok0705 <leeseon0503@gmail.com>
2023-10-29 22:49:31 -0400

    storage from backend local to AWS S3

--- server/middleware/imageUpload.js
+++ server/middleware/imageUpload.js
@@ -1,12 +1,23 @@
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
+const multerS3 = require("multer-s3");
+const { s3 } = require("../aws");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "./uploads"),
-  filename: (req, file, cb) =>
-    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+// 로컬 스토리지.
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => cb(null, "./uploads"),
+//   filename: (req, file, cb) =>
+//     cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+// });
+// S3 스토리지.
+const storage = multerS3({
+  s3,
+  bucket: "image-upload-storage",
+  key: (req, file, cb) =>
+    cb(null, `raw/${uuid()}.${mime.extension(file.mimetype)}`),
 });
+
 const upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
