30a805a86f0500b9c30c8a168e6443760845c86d
rok0705 <leeseon0503@gmail.com>
2023-11-01 01:31:04 -0400

    lambda fuc imageResizer(), nodejs from to 16

---
+++ lambda/index.js
@@ -0,0 +1,60 @@
+const sharp = require("sharp");
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+
+const transformationOptions = [
+  {
+    name: "w140",
+    width: 140,
+  },
+  {
+    name: "w600",
+    width: 600,
+  },
+];
+
+exports.handler = async (event) => {
+  try {
+    const Key = event.Records[0].s3.object.key;
+    console.log("Key:", Key);
+    const keyOnly = Key.split("/")[1];
+    console.log(`Image Resizing: ${keyOnly}`);
+    const image = await s3
+      .getObject({
+        Bucket: "image-upload-storage",
+        Key,
+      })
+      .promise();
+    await Promise.all(
+      transformationOptions.map(async ({ name, width }) => {
+        try {
+          const newKey = `${name}/${keyOnly}`;
+          const resizedImage = await sharp(image.Body)
+            .rotate()
+            .resize(width)
+            .toBuffer();
+          await s3
+            .putObject({
+              Bucket: "image-upload-storage",
+              Body: resizedImage,
+              Key: newKey,
+            })
+            .promise();
+        } catch (err) {
+          throw err;
+        }
+      })
+    );
+
+    return {
+      statusCode: 200,
+      body: event,
+    };
+  } catch (err) {
+    console.log(`err:${err}`);
+    return {
+      statusCode: 500,
+      body: event,
+    };
+  }
+};
